54020ab14d4d8c7e38de7e67e357acf3a2f2d947
Daniel Friesel <derf@finalrewind.org>
2012-03-06 09:35:44 +0100

    main loop, PWM, button readout

--- main.c
+++ main.c
@@ -6,7 +6,12 @@
 
 #define BRIGHTNESS_MAX 40
 
-volatile unsigned int int_en = 0;
+volatile unsigned char btn = 0;
+volatile unsigned char red, green, yellow;
+
+unsigned char pwm[16] = {
+	0, 2, 3, 4, 6, 8, 11, 16, 23, 32, 45, 64, 90, 128, 181, 255
+};
 
 inline void set_led(char red, char green, char blue)
 {
@@ -15,6 +20,13 @@ inline void set_led(char red, char green, char blue)
 
 int main (void)
 {
+
+	unsigned char cur = 0;
+
+	red = 10;
+	yellow = 14;
+	green = 15;
+
 	/* in / out */
 	DDRD = _BV(DDD2) | _BV(DDD3) | _BV(DDD4) | _BV(DDD5);
 	DDRB = 0;
@@ -22,37 +34,36 @@ int main (void)
 	/* enable internal pull-ups */
 	PORTB = _BV(PB0) | _BV(PB1) | _BV(PB2);
 
-	cli();
-
-	/* enable pin change interrupts */
-	PCMSK = _BV(PCINT0) | _BV(PCINT1) | _BV(PCINT2);
-	GIMSK = _BV(PCIE);
-
 	/* PWM */
 	OCR0A = 0xff;
-	TCCR0A = _BV(COM0B0) | _BV(WGM01);
-	TCCR0B = _BV(CS01) | _BV(CS00); /* /64 */
+	TCCR0A = _BV(COM0B0) | _BV(WGM01); /* OC0B, count from 0 to OCR0A */
+	/* TCCR0B = _BV(CS01) | _BV(CS00); */ /* /64 prescaler */
 
 	PORTD |= _BV(PD4);
 
 	sei();
 	while (1) {
-		if (++int_en == 0) {
-			sei();
-			PIND = _BV(PD2) | _BV(PD3) | _BV(PD4);
-			OCR0A++;
+		cur++;
+
+		if (cur == 0) {
+			PORTD |= _BV(PD2) | _BV(PD3) | _BV(PD4);
+			if (++btn == 32) {
+				btn = 0;
+				if (~PINB & _BV(PB0))
+					green = (green + 1) % 16;
+				if (~PINB & _BV(PB1))
+					yellow = (yellow + 1) % 16;
+				if (~PINB & _BV(PB2))
+					red = (red + 1) % 16;
+			}
 		}
+		if (cur == pwm[red])
+			PORTD &= ~_BV(PD4);
+		if (cur == pwm[yellow])
+			PORTD &= ~_BV(PD3);
+		if (cur == pwm[green])
+			PORTD &= ~_BV(PD2);
 	}
 
 	return 0;
-}
-
-ISR(PCINT_vect)
-{
-	cli();
-	if (PIND & _BV(PD4))
-		PORTD &= ~_BV(PD4);
-	else
-		PORTD |= _BV(PD4);
-	int_en = 0;
 }
