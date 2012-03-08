a8d70c049b03d824c184eebbd5f0842b703b670f
Daniel Friesel <derf@finalrewind.org>
2012-03-08 18:57:15 +0100

    Run at 8MHz, disable unused AC

--- main.c
+++ main.c
@@ -4,32 +4,30 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define BRIGHTNESS_MAX 40
-
-volatile unsigned char btn = 0;
-volatile unsigned char red, green, yellow;
-
 unsigned char pwm[16] = {
 	0, 1, 1, 2, 3, 4, 5, 8, 12, 16, 22, 32, 45, 64, 91, 127
 };
 
-inline void set_led(char red, char green, char blue)
-{
-	PORTD = (red << PD4) | (blue << PD5) | (green << PD6);
-}
-
 int main (void)
 {
 
-	unsigned char cur = 0;
+	unsigned char cur = 0, btn = 0, skip = 0;
+	unsigned char red, green, yellow;
 
-	red = 1;
+	red = 3;
 	yellow = 15;
 	green = 15;
+
+	/* run at 8 MHz */
+	CLKPR = _BV(CLKPCE);
+	CLKPR = 0;
 
 	/* watchdog reset after ~8 seconds */
 	MCUSR = 0;
 	WDTCSR = _BV(WDE) | _BV(WDP3) | _BV(WDP0);
+
+	/* disable analog comparator to reduce power consumption */
+	ACSR |= _BV(ACD);
 
 	/* in / out */
 	DDRD = _BV(DDD2) | _BV(DDD3) | _BV(DDD4) | _BV(DDD5);
@@ -50,23 +48,27 @@ int main (void)
 
 		if (cur == 0) {
 			PORTD |= _BV(PD2) | _BV(PD3) | _BV(PD4);
-			if (++btn == 16) {
+			if (++btn == 200) {
 				asm("wdr");
 				btn = 0;
-				if (~PINB & _BV(PB0)) {
-					green = (green + 1) % 16;
-					if (green == 15)
-						btn = 128;
-				}
-				if (~PINB & _BV(PB1)) {
-					yellow = (yellow + 1) % 16;
-					if (yellow == 15)
-						btn = 128;
-				}
-				if (~PINB & _BV(PB2)) {
-					red = (red + 1) % 16;
-					if (red == 15)
-						btn = 128;
+				if (skip)
+					skip--;
+				else {
+					if (~PINB & _BV(PB0)) {
+						green = (green + 1) % 16;
+						if (green == 15)
+							skip = 10;
+					}
+					if (~PINB & _BV(PB1)) {
+						yellow = (yellow + 1) % 16;
+						if (yellow == 15)
+							skip = 10;
+					}
+					if (~PINB & _BV(PB2)) {
+						red = (red + 1) % 16;
+						if (red == 15)
+							skip = 10;
+					}
 				}
 			}
 		}
